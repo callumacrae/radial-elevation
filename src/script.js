5a4613d35e2212c8c5cb0aa9f7d451fd3acd5eb3
Callum Macrae <callum@samknows.com>
2016-12-10 20:42:08 +0000

    OO

--- src/script.js
+++ src/script.js
@@ -10,217 +10,252 @@ Promise.all([
 	getGpxData('../data/LEJOG_Day_6_All_done_.gpx')
 ])
 	.then((datas) => {
-		const svg = d3.select('svg');
-		const width = svg.attr('width');
-		const height = svg.attr('height');
+		const chart = new RadialElevation('svg', datas);
+		chart.preprocessData();
+		chart.drawAxis();
+		chart.drawDaySections();
+		chart.drawDistanceMarkers();
+		chart.drawSplatter();
+		chart.drawMaxElevations();
+		chart.drawCentralTitle();
+	});
 
-		const origin = { x: 400, y: 400, toString() {
+function RadialElevation(selector, datas) {
+	this.svg = d3.select(selector);
+
+	this.config = {
+		origin: { x: 400, y: 400, toString() {
 			return `${this.x} ${this.y}`;
-		}};
-
-		const flip = {
+		}},
+		flip: {
 			enabled: false,
 			min: 112.5,
 			max: 237.5
-		};
+		}
+	};
 
-		const totalDist = datas.reduce((total, { points }) => total + points[points.length - 1].dist, 0);
+	this.datas = datas;
 
-		const distanceScale = d3.scaleLinear()
-			.domain([0, totalDist])
-			.range([-Math.PI / 2, Math.PI / 2 * 3]);
+	this.totalDist = datas.reduce((total, { points }) => total + points[points.length - 1].dist, 0);
 
-		const extent = datas.reduce(([min, max], { points }) => {
-			return points.reduce(([innerMin, innerMax], { ele }) => {
-				return [Math.min(ele, innerMin), Math.max(ele, innerMax)];
-			}, [min, max]);
-		}, [Infinity, 0]);
+	const distanceScale = d3.scaleLinear()
+		.domain([0, this.totalDist])
+		.range([-Math.PI / 2, Math.PI / 2 * 3]);
 
-		const elevationScale = d3.scaleLinear()
-			.domain(extent)
-			.range([150, 350]);
+	const extent = datas.reduce(([min, max], { points }) => {
+		return points.reduce(([innerMin, innerMax], { ele }) => {
+			return [Math.min(ele, innerMin), Math.max(ele, innerMax)];
+		}, [min, max]);
+	}, [Infinity, 0]);
 
-		const tickVals = [];
+	const elevationScale = d3.scaleLinear()
+		.domain(extent)
+		.range([150, 350]);
 
-		const axis = d3.axisRight(elevationScale.copy().range(elevationScale.range().reverse()))
-			.ticks(4)
-			.tickFormat((num) => {
-				tickVals.push(num);
-				return `${num}m`;
-			});
+	this.scales = {
+		distance: distanceScale,
+		elevation: elevationScale
+	};
+}
 
-		svg.append('g')
-			.call(axis)
-			.attr('class', 'axis')
-			.attr('transform', `translate(${origin.x} ${origin.y - 500})`)
-			.selectAll('text')
-			.attr('transform', 'rotate(3)');
+RadialElevation.prototype.preprocessData = function () {
+	let offset = 0;
+	let path = '';
 
-		const arc = d3.arc()
-			.innerRadius((d) => elevationScale(d) - 1)
-			.outerRadius(elevationScale)
-			.startAngle((d) => 37 / elevationScale(d))
-			.endAngle(Math.PI * 2);
+	this.datas.forEach(({ meta, points }) => {
+		let maxPoint = { ele: 0 };
 
-		svg.selectAll('.circle-axis')
-			.data(tickVals)
-			.enter()
-			.append('path')
-			.attr('class', 'circle-axis')
-			.attr('d', arc)
-			.attr('transform', `translate(${origin})`);
+		const pathSegment = points.map((d) => {
+			if (d.ele > maxPoint.ele) {
+				maxPoint = d;
+			}
 
-		let offset = 0;
-		let path = '';
+			const x = dp(this.config.origin.x + this.scales.elevation(d.ele) * Math.cos(this.scales.distance(d.dist + offset)), 1);
+			const y = dp(this.config.origin.y + this.scales.elevation(d.ele) * Math.sin(this.scales.distance(d.dist + offset)), 1);
 
-		datas.forEach(({ meta, points }) => {
-			let maxPoint = { ele: 0 };
+			return `L ${x} ${y}`;
+		}).join(' ');
 
-			const pathSegment = points.map((d) => {
-				if (d.ele > maxPoint.ele) {
-					maxPoint = d;
-				}
+		path += pathSegment + ' ';
 
-				const x = dp(origin.x + elevationScale(d.ele) * Math.cos(distanceScale(d.dist + offset)), 1);
-				const y = dp(origin.y + elevationScale(d.ele) * Math.sin(distanceScale(d.dist + offset)), 1);
+		meta.startAngle = offset;
+		offset += points[points.length - 1].dist;
+		meta.endAngle = offset;
 
-				return `L ${x} ${y}`;
-			}).join(' ');
+		meta.maxPoint = maxPoint;
+	});
 
-			path += pathSegment + ' ';
+	this.pathData = path;
+};
 
-			meta.startAngle = offset;
-			offset += points[points.length - 1].dist;
-			meta.endAngle = offset;
+RadialElevation.prototype.drawAxis = function () {
+	const tickVals = this.tickVals = [];
 
-			meta.maxPoint = maxPoint;
+	const reverseScale = this.scales.elevation.copy().range(this.scales.elevation.range().reverse());
+
+	const axis = d3.axisRight(reverseScale)
+		.ticks(4)
+		.tickFormat((num) => {
+			tickVals.push(num);
+			return `${num}m`;
 		});
 
-		const days = svg.selectAll('.day')
-			.data(datas)
-			.enter()
-			.append('g');
+	this.svg.append('g')
+		.call(axis)
+		.attr('class', 'axis')
+		.attr('transform', `translate(${this.config.origin.x} ${this.config.origin.y - 500})`)
+		.selectAll('text')
+		.attr('transform', 'rotate(3)');
 
-		days.append('path')
-			.attr('d', ({ meta }) => {
-				const angle = distanceScale(meta.startAngle);
+	const arc = d3.arc()
+		.innerRadius((d) => this.scales.elevation(d) - 1)
+		.outerRadius(this.scales.elevation)
+		.startAngle((d) => 37 / this.scales.elevation(d))
+		.endAngle(Math.PI * 2);
 
-				const radius = elevationScale(tickVals[tickVals.length - 1]);
+	this.svg.selectAll('.circle-axis')
+		.data(tickVals)
+		.enter()
+		.append('path')
+		.attr('class', 'circle-axis')
+		.attr('d', arc)
+		.attr('transform', `translate(${this.config.origin})`);
+};
 
-				const x = dp(radius * Math.cos(angle), 1);
-				const y = dp(radius * Math.sin(angle), 1);
+RadialElevation.prototype.drawDaySections = function () {
+	const days = this.svg.selectAll('.day')
+		.data(this.datas)
+		.enter()
+		.append('g');
 
-				return `M ${origin} l ${x} ${y}`;
-			})
-			.attr('class', 'divider');
+	days.append('path')
+		.attr('d', ({ meta }) => {
+			const angle = this.scales.distance(meta.startAngle);
 
-		const dayTitles = days.append('g')
-			.attr('transform', ({ meta }) => {
-				const halfwayAngle = distanceScale((meta.startAngle + meta.endAngle) / 2);
-
-				const textX = origin.x + dp(365 * Math.cos(halfwayAngle), 1);
-				const textY = origin.y + dp(365 * Math.sin(halfwayAngle), 1);
-
-				let degs = halfwayAngle / Math.PI * 180 + 90;
-
-				if (flip.enabled && (degs > flip.min && degs < flip.max)) {
-					degs -= 180;
-				}
-
-				return `translate(${textX}, ${textY}) rotate(${degs})`;
-			});
-
-		dayTitles.append('text')
-			.text(({ meta }) => meta.name.toUpperCase())
-			.attr('class', 'day')
-			.attr('y', 4);
-
-		dayTitles.append('text')
-			.text(({ meta }) => `${meta.distance} miles`)
-			.attr('class', 'meta')
-			.attr('y', 13);
-
-		// Draw line every 160934 metres, which is a mile
-		for (let i = 160934; i < totalDist; i += 160934) {
-			const angle =	distanceScale(i);
-
-			const radius = elevationScale(tickVals[tickVals.length - 1]);
+			const radius = this.scales.elevation(this.tickVals[this.tickVals.length - 1]);
 
 			const x = dp(radius * Math.cos(angle), 1);
 			const y = dp(radius * Math.sin(angle), 1);
 
-			svg.append('path')
-				.attr('d', `M ${origin} l ${x} ${y}`)
-				.attr('class', 'distance-divider');
+			return `M ${this.config.origin} l ${x} ${y}`;
+		})
+		.attr('class', 'divider');
 
-			let degs = angle / Math.PI * 180 + 90;
+	const dayTitles = days.append('g')
+		.attr('transform', ({ meta }) => {
+			const halfwayAngle = this.scales.distance((meta.startAngle + meta.endAngle) / 2);
 
-			if (flip.enabled && (degs > flip.min && degs < flip.max)) {
+			const textX = this.config.origin.x + dp(365 * Math.cos(halfwayAngle), 1);
+			const textY = this.config.origin.y + dp(365 * Math.sin(halfwayAngle), 1);
+
+			let degs = halfwayAngle / Math.PI * 180 + 90;
+
+			if (this.config.flip.enabled && (degs > this.config.flip.min && degs < this.config.flip.max)) {
 				degs -= 180;
 			}
 
-			svg.append('text')
-				.text(i / 1609.34 + ' miles')
-				.attr('class', 'meta meta-distance')
-				.attr('transform', `translate(${origin.x + x * 1.02}, ${origin.y + y * 1.02}) rotate(${degs})`);
+			return `translate(${textX}, ${textY}) rotate(${degs})`;
+		});
+
+	dayTitles.append('text')
+		.text(({ meta }) => meta.name.toUpperCase())
+		.attr('class', 'day')
+		.attr('y', 4);
+
+	dayTitles.append('text')
+		.text(({ meta }) => `${meta.distance} miles`)
+		.attr('class', 'meta')
+		.attr('y', 13);
+};
+
+RadialElevation.prototype.drawDistanceMarkers = function () {
+	// Draw line every 160934 metres, which is a mile
+	for (let i = 160934; i < this.totalDist; i += 160934) {
+		const angle =	this.scales.distance(i);
+
+		const radius = this.scales.elevation(this.tickVals[this.tickVals.length - 1]);
+
+		const x = dp(radius * Math.cos(angle), 1);
+		const y = dp(radius * Math.sin(angle), 1);
+
+		this.svg.append('path')
+			.attr('d', `M ${this.config.origin} l ${x} ${y}`)
+			.attr('class', 'distance-divider');
+
+		let degs = angle / Math.PI * 180 + 90;
+
+		if (this.config.flip.enabled && (degs > this.config.flip.min && degs < this.config.flip.max)) {
+			degs -= 180;
 		}
 
-		svg.append('path')
-			.attr('d', `M ${path.slice(1)}`)
-			.attr('class', 'data');
+		this.svg.append('text')
+			.text(i / 1609.34 + ' miles')
+			.attr('class', 'meta meta-distance')
+			.attr('transform', `translate(${this.config.origin.x + x * 1.02}, ${this.config.origin.y + y * 1.02}) rotate(${degs})`);
+	}
+};
 
-		const groups = svg.selectAll('.max-ele')
-			.data(datas)
-			.enter()
-			.append('g')
-			.attr('transform', ({ meta }) => {
-				const maxAngle = distanceScale(meta.startAngle + meta.maxPoint.dist);
-				const maxRadius = elevationScale(meta.maxPoint.ele) - 3;
+RadialElevation.prototype.drawSplatter = function () {
+	this.svg.append('path')
+		.attr('d', `M ${this.pathData.slice(1)}`)
+		.attr('class', 'data');
+};
 
-				const maxX = origin.x + dp(maxRadius * Math.cos(maxAngle), 1);
-				const maxY = origin.y + dp(maxRadius * Math.sin(maxAngle), 1);
+RadialElevation.prototype.drawMaxElevations = function () {
+	const groups = this.svg.selectAll('.max-ele')
+		.data(this.datas)
+		.enter()
+		.append('g')
+		.attr('transform', ({ meta }) => {
+			const maxAngle = this.scales.distance(meta.startAngle + meta.maxPoint.dist);
+			const maxRadius = this.scales.elevation(meta.maxPoint.ele) - 3;
 
-				// Rotate by an extra 5deg
-				let degs = maxAngle / Math.PI * 180 + 95;
+			const maxX = this.config.origin.x + dp(maxRadius * Math.cos(maxAngle), 1);
+			const maxY = this.config.origin.y + dp(maxRadius * Math.sin(maxAngle), 1);
 
-				if (flip.enabled && (degs > flip.min && degs < flip.max)) {
-					// Do previous rotation in other direction: -10
-					degs = degs - 190;
-				}
+			// Rotate by an extra 5deg
+			let degs = maxAngle / Math.PI * 180 + 95;
 
-				return `translate(${maxX}, ${maxY}) rotate(${degs})`;
-			})
-			.attr('class', 'max-ele');
+			if (this.config.flip.enabled && (degs > this.config.flip.min && degs < this.config.flip.max)) {
+				// Do previous rotation in other direction: -10
+				degs = degs - 190;
+			}
 
-		// Text background
-		groups.append('rect')
-			.attr('width', 37)
-			.attr('height', 10)
-			.attr('x', 3)
-			.attr('y', -6);
+			return `translate(${maxX}, ${maxY}) rotate(${degs})`;
+		})
+		.attr('class', 'max-ele');
 
-		groups.append('text')
-			.text(({ meta }) => meta.maxPoint.ele + 'm')
-			.attr('x', 5);
+	// Text background
+	groups.append('rect')
+		.attr('width', 37)
+		.attr('height', 10)
+		.attr('x', 3)
+		.attr('y', -6);
 
-		svg.append('text')
-			.text('SIX DAYS')
-			.attr('class', 'title')
-			.attr('x', origin.x)
-			.attr('y', origin.y - 30);
+	groups.append('text')
+		.text(({ meta }) => meta.maxPoint.ele + 'm')
+		.attr('x', 5);
+};
 
-		svg.append('text')
-			.text('1,018 MILES RIDDEN')
-			.attr('class', 'title')
-			.attr('x', origin.x)
-			.attr('y', origin.y + 10);
+RadialElevation.prototype.drawCentralTitle = function () {
+	this.svg.append('text')
+		.text('SIX DAYS')
+		.attr('class', 'title')
+		.attr('x', this.config.origin.x)
+		.attr('y', this.config.origin.y - 30);
 
-		svg.append('text')
-			.text('50,048ft CLIMBED')
-			.attr('class', 'title')
-			.attr('x', origin.x)
-			.attr('y', origin.y + 50);
-	});
+	this.svg.append('text')
+		.text('1,018 MILES RIDDEN')
+		.attr('class', 'title')
+		.attr('x', this.config.origin.x)
+		.attr('y', this.config.origin.y + 10);
+
+	this.svg.append('text')
+		.text('50,048ft CLIMBED')
+		.attr('class', 'title')
+		.attr('x', this.config.origin.x)
+		.attr('y', this.config.origin.y + 50);
+};
 
 function dp(num, figs) {
 	return Math.round(num * Math.pow(10, figs)) / Math.pow(10, figs);
